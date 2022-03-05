017489f4f37986ca765dce5671eb4a93dbc48289
Ismail Marmoush <marmoushismail@gmail.com>
2022-03-05 01:00:08 +0100

    size test

--- mkafka/src/test/java/io/memoria/mkafka/DefaultKafkaStreamTest.java
+++ mkafka/src/test/java/io/memoria/mkafka/DefaultKafkaStreamTest.java
@@ -4,11 +4,14 @@ import io.memoria.reactive.core.id.Id;
 import io.memoria.reactive.core.stream.Msg;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 
 import java.util.Random;
 
+@TestMethodOrder(OrderAnnotation.class)
 class DefaultKafkaStreamTest {
   private static final Random random = new Random();
   private static final int MSG_COUNT = 1000;
@@ -22,6 +25,13 @@ class DefaultKafkaStreamTest {
 
   @Test
   @Order(0)
+  void sizeBefore() {
+    var size = repo.size(TOPIC, PARTITION);
+    StepVerifier.create(size).expectNext(0L).verifyComplete();
+  }
+
+  @Test
+  @Order(1)
   void publish() {
     // Given
     var msgs = Flux.range(0, MSG_COUNT).map(i -> new Msg(TOPIC, PARTITION, Id.of(i), "hello" + i));
@@ -34,12 +44,19 @@ class DefaultKafkaStreamTest {
   }
 
   @Test
-  @Order(1)
+  @Order(2)
   void subscribe() {
     // Given previous publish ran successfully
     // When
     var sub = repo.subscribe(TOPIC, PARTITION, 0).take(MSG_COUNT);
     // Given
     StepVerifier.create(sub).expectNextCount(MSG_COUNT).verifyComplete();
+  }
+
+  @Test
+  @Order(3)
+  void sizeAfter() {
+    var size = repo.size(TOPIC, PARTITION);
+    StepVerifier.create(size).expectNext(1000L).verifyComplete();
   }
 }
